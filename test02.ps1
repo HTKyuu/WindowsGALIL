5e56c846c2e7422eff500bb3b0d6b849ee0b65a0
HTKyuu <79772156+HTKyuu@users.noreply.github.com>
2025-08-26 14:37:08 +0200

    Create test02.ps1

---
+++ test02.ps1
@@ -0,0 +1,6 @@
+# Check if running as Administrator
+If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Error "This script must be run as Administrator."
+    exit
+}
+print("Hello, World!")
