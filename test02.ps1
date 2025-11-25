ee29484883bf4776d0951f49d4477d4b8b3641b8
HTKyuu <79772156+HTKyuu@users.noreply.github.com>
2025-11-25 03:38:14 +0100

    Delete test02.ps1

--- test02.ps1
+++
@@ -1,8 +0,0 @@
-# Check if running as Administrator
-If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Error "This script must be run as Administrator."
-    exit
-}
-
-
-[Console]::WriteLine("Hello, World!")
